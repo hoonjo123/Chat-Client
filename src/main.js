583103b8aa4432c5b1ac3a2dc2f4d1350a6b9354
hoonjo123 <johoon030@gmail.com>
2025-07-20 00:19:59 +0900

    feat:회원파트 구현 완료

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import router from './router'
+import axios from 'axios';
 
-createApp(App).mount('#app')
+createApp(App).use(router).use(vuetify).mount('#app')
+
+axios.interceptors.request.use(
+    config => {
+        const token = localStorage.getItem("token");
+        if(token){
+            config.headers['Authorization'] = `Bearer ${token}`
+        }
+        return config;
+    },
+    error => {
+        //인터셉터를 무시하고, 사용자의 본래요청인 화면으로 라우팅
+        return Promise.reject(error);
+    }
+)
